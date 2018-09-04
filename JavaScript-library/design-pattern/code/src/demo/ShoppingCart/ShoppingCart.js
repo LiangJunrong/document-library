0b6314be4b06639079a1414332a6806bdf455339
jsliang <1741020489@qq.com>
2018-09-04 16:41:36 +0800

    设计模式综合应用

--- JavaScript-library/design-pattern/code/src/demo/ShoppingCart/ShoppingCart.js
+++ JavaScript-library/design-pattern/code/src/demo/ShoppingCart/ShoppingCart.js
@@ -1,9 +1,34 @@
+import $ from 'jquery';
+import getCart from './GetCart.js'
+
 export default class ShoppingCart {
     constructor(app) {
+        this.app = app;
+        this.$el = $('<div>').css({
+            'padding-bottom': '10px;',
+            'border-bottom': '1px solid #ccc'
+        })
+        this.cart = getCart();
+    }
 
+    initBtn() {
+        let $btn = $('<button>购物车</button>');
+        $btn.click(() => {
+            this.showCart();
+        })
+        this.$el.append($btn);
+    }
+
+    showCart() {
+        alert(this.cart.getList());
+    }
+
+    render() {
+        this.app.$el.append(this.$el);
     }
 
     init() {
-        
+        this.initBtn();
+        this.render();
     }
 }
