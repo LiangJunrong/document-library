49917cd7b98f8fa9270d4ef14bf2758d9a16908c
jsliang <1741020489@qq.com>
2018-09-10 17:53:32 +0800

    react 学习

--- JavaScript-library/React/code/jianshu/src/common/header/index.js
+++ JavaScript-library/React/code/jianshu/src/common/header/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-    HeaderWrapper, Logo, Nav, NavItem
+    HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button
 } from './style'
 
 class Header extends Component {
@@ -13,7 +13,12 @@ class Header extends Component {
                     <NavItem className="left">下载App</NavItem>
                     <NavItem className="right">登录</NavItem>
                     <NavItem className="right">Aa</NavItem>
+                    <NavSearch></NavSearch>
                 </Nav>
+                <Addition>
+                    <Button className="writting">写文章</Button>
+                    <Button className="reg">注册</Button>
+                </Addition>
             </HeaderWrapper>
         )
     }
--- JavaScript-library/React/code/jianshu/src/common/header/style.js
+++ JavaScript-library/React/code/jianshu/src/common/header/style.js
@@ -5,7 +5,7 @@ export const HeaderWrapper = styled.div`
     position: relative;
     height: 56px;
     border-bottom: 1px solid #f0f0f0;
-`
+`;
 
 export const Logo = styled.a.attrs({
     href: '/'
@@ -18,13 +18,13 @@ export const Logo = styled.a.attrs({
     height: 56px;
     background: url(${logoPic});
     background-size: contain;
-`
+`;
 
 export const Nav = styled.div`
     width: 960px;
     height: 100%;
     margin: 0 auto;
-`
+`;
 
 export const NavItem = styled.div`
     line-height: 56px;
@@ -40,5 +40,45 @@ export const NavItem = styled.div`
     }
     &.active {
         color: #ea6f5a;
+    }
+`;
+
+export const NavSearch = styled.input.attrs({
+    placeholder: '搜索'
+})`
+    width: 160px;
+    height: 38px;
+    padding: 0 20px;
+    margin-top: 9px;
+    margin-left: 20px;
+    border: none;
+    outline: none;
+    border-radius: 19px;
+    background: #eee;
+    font-size: 14px;
+`;
+
+export const Addition = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 56px;
+`;
+
+export const Button = styled.div`
+    float: right;
+    margin-top: 9px;
+    margin-right: 20px;
+    padding: 0 20px;
+    line-height: 38px; 
+    border-radius: 19px;
+    border: 1px solid #ec6149;
+    font-size: 14px;
+    &.reg {
+        color: #ec6149;
+    }
+    &.writting {
+        color: #fff;
+        background: #ec6149;
     }
 `
