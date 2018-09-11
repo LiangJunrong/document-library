523c4abc47127a09a679189a4e6cd8b54ef9f66a
jsliang <1741020489@qq.com>
2018-09-11 13:55:59 +0800

    react 学习

--- JavaScript-library/React/code/jianshu/src/common/header/store/reducer.js
+++ JavaScript-library/React/code/jianshu/src/common/header/store/reducer.js
@@ -7,6 +7,7 @@ const defaultState = fromJS({
 
 export default (state = defaultState, action) => {
     if(action.type === actionTypes.SEARCH_FOCUS) {
+        // immutable 对象的 set 方法，会结合之前的 immutable 对象的值和设置的值，返回一个全新的对象
         return state.set('focused', true);
     }
     if(action.type === actionTypes.SEARCH_BLUR) {
