83f7ff9c27c96ea73dcfcb71f1af43f581805a52
jsliang <1741020489@qq.com>
2018-10-19 13:48:44 +0800

    Vue + Node + MongoDB 学习笔记

---
+++ other-library/myBlog/code/shopping-mall/README.md
@@ -0,0 +1,17 @@
+商城
+===
+
+> 使用 Vue 搭建的商城
+
+## 运行机制
+
+```
+# 安装依赖
+npm install
+
+# 在 localhost:8080 运行
+npm run dev
+
+# 打包部署到服务器
+npm run build
+```
---
+++ other-library/myBlog/code/shopping-mall/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
